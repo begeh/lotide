5c0defc8de7e1a53dcb43a074b39841419c12beb
Bashir Egeh <b_egeh@hotmail.com>
2020-01-07 23:27:19 +0000

    First commit

---
+++ flatten.js
@@ -0,0 +1,38 @@
+const eqArrays = function(actual, expected) {
+  let result;
+  if (actual.length !== expected.length) {
+    result = false;
+  } else {
+    result = true;
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        result = false;
+      }
+    }
+  }
+  return result;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`😀😀😀Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😔😔😔Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const flatten = function(array) {
+  let arr = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i]) === true) {
+      for (let j = 0; j < array[i].length; j++) {
+        arr.push(array[i][j]);
+      }
+    } else {
+      arr.push(array[i]);
+    }
+  }
+  return arr;
+};
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); // => [1, 2, 3, 4, 5, 6] PASSED
