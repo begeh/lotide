48b01e9093e9d0899fe6aaae841d35cc538f6919
Bashir Egeh <b_egeh@hotmail.com>
2020-01-07 20:52:38 +0000

    Edits and initial push assertArrayEquals

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -18,11 +18,10 @@ const eqArrays = function(actual, expected) {
       }
     }
   }
-  console.log(result);
+  return result;
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
