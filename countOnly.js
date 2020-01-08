aacbe947bd4035991006f88b32db13d014713e66
Bashir Egeh <b_egeh@hotmail.com>
2020-01-08 18:51:32 +0000

    Edited file

--- countOnly.js
+++ countOnly.js
@@ -7,19 +7,18 @@ const assertEqual = function(actual, expected) {
 };
 
 const countOnly = function(allItems, itemsToCount) {
-  const objectKeys = Object.keys(itemsToCount);
   const results = {};
-
   for (const item of allItems) {
-    if (results[item]) {
-      results[item] += 1;
-    } else {
-      results[item] = 1;
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
     }
   }
-
   return results;
-}
+};
 
 const firstNames = [
   "Karl",
