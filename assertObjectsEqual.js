5642751e0285f2e84078adabe3c2fd732cb8ce31
Bashir Egeh <b_egeh@hotmail.com>
2020-01-08 21:17:09 +0000

    First commit

---
+++ assertObjectsEqual.js
@@ -0,0 +1,49 @@
+
+
+const eqObjects = function(object1, object2) {
+  let obj1 = Object.keys(object1);
+  let obj2 = Object.keys(object2);
+  if (obj1.length === obj2.length) {
+    for (let key of obj1) {
+      if (Array.isArray(object1[key]) || Array.isArray(object2[key])) {
+        return eqArrays(object1[key], object2[key]);
+      }
+      if (object2[key]) {
+        if (object1[key] !== object2[key]) {
+          return false;
+        }
+      } else {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const eqArrays = function(actual, expected) {
+  let result;
+  if (actual.length !== expected.length) {
+    result = false;
+  } else {
+    result = true;
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        result = false;
+      }
+    }
+  }
+  return result;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`😀😀😀Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`😔😔😔Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
+};
+
+assertObjectsEqual({ c: "1", d: ["2", 3] }, { c: "1", d: ["2", 3] });
