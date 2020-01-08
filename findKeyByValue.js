17a2ec482620af2cbde788864736c53df6165851
Bashir Egeh <b_egeh@hotmail.com>
2020-01-08 18:33:54 +0000

    First commit

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😀😀😀Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😔😔😔Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(list, show) {
+  let arr = Object.entries(list);
+  for (let item of arr) {
+    if (item[1] === show) {
+      return item[0];
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
