a8d22716c9187a0e2bc8fcdfe6bf2cc43d62ecf3
Bashir Egeh <b_egeh@hotmail.com>
2020-01-08 18:26:42 +0000

    Fixed assertEqual function check

--- countLetter.js
+++ countLetter.js
@@ -20,3 +20,5 @@ const countLetters = function(arg) {
 }
 const args = process.argv.slice(2);;
 console.log(countLetters(args[0]));
+//args[0] is the word password, which we input each time
+assertEqual((countLetters(args[0]).p), 1);
